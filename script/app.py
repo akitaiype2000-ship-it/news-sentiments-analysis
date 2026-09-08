7d387a35c934c6906a9c846a7bdd82c3738a0e5a
akitaiype2000-ship-it <akitaiypecherian@gmail.com>
2026-09-08 22:39:54 +0530

    Implement sentiment analysis using TextBlob

--- script/app.py
+++ script/app.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+from textblob import TextBlob
 api_key = "ba901fc4867d4cc3b1dcfc0f87949b3e"
 
 url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}"
@@ -11,6 +11,9 @@ with open("news_data.json", "w") as file:
     json.dump(data, file, indent=4)
 
 print("News saved locally")
+pos = 0
+neg = 0
+neut = 0
 for article in data["articles"]:
 
     title = article["title"]
@@ -19,12 +22,31 @@ for article in data["articles"]:
     description = article["description"]
     published = article["publishedAt"]
     article_url = article["url"]
+    # Sentiment Analysis
+    blob = TextBlob(title)
+    polarity = blob.sentiment.polarity
 
+    if polarity > 0:
+        sentiment = "Positive"
+        pos += 1
+    elif polarity < 0:
+        sentiment = "Negative"
+        neg += 1
+    else:
+        sentiment = "Neutral"
+        neut += 1
     print("Title:", title)
     print("Source:", source)
     print("Author:", author)
     print("Published:", published)
     print("Description:", description)
     print("URL:", article_url)
+    print("Sentiment:", sentiment)
+    print("Polarity:", polarity)
     print("-" * 80)
+print("\n========== SUMMARY ==========")
+print("Total Articles:", len(data["articles"]))
+print("Positive:", pos)
+print("Negative:", neg)
+print("Neutral:", neut)
 print(data)
