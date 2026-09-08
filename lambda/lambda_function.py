912bba07c78fda769916cee5f69cfde6fd5d9e06
akitaiype2000-ship-it <akitaiypecherian@gmail.com>
2026-09-08 23:02:59 +0530

    Fetch latest news using NewsAPI in Lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,17 @@
+import os
 import json
+import requests
+
+NEWS_API_KEY = os.environ["NEWS_API_KEY"]
 
 def lambda_handler(event, context):
 
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={NEWS_API_KEY}"
+
+    response = requests.get(url)
+    data = response.json()
+
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "Lambda function is running"
-        })
+        "body": json.dumps(data)
     }
